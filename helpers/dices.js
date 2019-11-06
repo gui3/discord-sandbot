671e48b74ba04741a9c440cbae2de825e09ed8f1
guillaume3 <guillaume.3.7.13@gmail.com>
2019-11-06 18:19:51 +0100

    bugfix

---
+++ helpers/dices.js
@@ -0,0 +1,15 @@
+module.exports = (string) => {
+  var [dices, faces] = argument.split("d").map(x => parseInt(x)),
+    result = {
+      dices: [],
+      sum:0
+    };
+
+  for (var i = 0; i< dices; i++) {
+    results.dices.push(Math.floor(Math.random()*faces+1));
+  };
+
+  results.sum = results.dices.reduce((a,b)=>a+b);
+
+  return reply
+}
