31d746409c0f14d14fb54e80622505b4054f29b7
guillaume3 <guillaume.3.7.13@gmail.com>
2019-11-08 20:32:38 +0100

    guild join messages

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-const Discord = require('discord.js')
-require('dotenv').config()
+const Discord = require('discord.js');
+const fs = require("fs");
+require('dotenv').config();
 
-const client = new Discord.Client()
+const client = new Discord.Client();
 
 // READY -------------------------------------
 client.on("ready", client => {
-  console.log(`Logged in !`)
+  console.log(`Logged in !`);
+  client.guilds.forEach(guild => {
+    console.log('Connected to guild : ' + guild.name);
+  });
 });
+
+//joined a server
+client.on("guildCreate", guild => {
+    console.log("Joined a new guild: " + guild.name);
+    //Your other stuff like adding to guildArray
+})
 
 
 // MESSAGES & COMMANDS -----------------------
-const interpreter = require("./commands/_interpreter")
+var commands = {};
+fs.readdir("./commands", (err, files) => {
+  files.forEach( file => {
+    try {
+      var command = require("./command/"+file.replace(/.js$/,""));
+      if (!(command.name && typeof command.function == "function" )) {
+        throw new Error("could not load command file " + file)
+      }
+      commands[command.name] = command;
+    } catch (e) {
+      console.log(e.message)
+    }
+  })
+})
+
 client.on("message", message => {
   interpreter(message, client);
 });
