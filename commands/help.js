829fd15e3ac3eed2e88139c31471b9a61e998a24
guillaume3 <guillaume.3.7.13@gmail.com>
2019-11-08 21:51:22 +0100

    bugfix final reorganization of commands

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,8 @@ module.exports = {
   function: function (arguments, message) {
     let reply = "";
     let prefix = message.guild.guildVars.prefix;
-    Object.keys(message.client.botVars.commands).forEach(command => {
+    Object.keys(message.client.botVars.commands).forEach(key => {
+      let command = message.client.botVars.commands[key];
       reply += "\n___ Commande "+prefix+command.name+" ___\n"+
         command.help ? command.help : "(pas d'aide sur cette commande ...)\n";
     });
