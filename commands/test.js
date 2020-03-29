b98d70ce5a2966a49465e595e4c0c0524f0d4cc3
gui3 <gui.silvent@gmail.com>
2020-03-29 17:08:30 +0200

    test coco debug

--- commands/test.js
+++ commands/test.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "test",
   help: "utilise ce template pour faire des fonctions",
-  function: function (arguments, message) {
+  function: function (arguments, message, debug) {
     // arguments est la liste de mots qui suivent la commande
     //   par exemple pour !roll 1d100, arguments = ["1d100"]
     // message est la référence envers le message discord qui a lancé la commande
@@ -10,5 +10,7 @@ module.exports = {
     message.reply("j'ai bien compris")
     message.reply("vous avez dit : " + arguments.join(" "))
     // Array.join 'colle' tous les éléments d'un Array avec le texte en argument
+
+    debug.say("je suis en mode debug")
   }
 };
