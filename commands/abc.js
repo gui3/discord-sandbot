793162e97e3d071fead8cabea31fa2379a1bebea
psi_thorn <guillon.corentin@gmail.com>
2020-03-30 17:18:41 +0200

    correction bugs abc.js

--- commands/abc.js
+++ commands/abc.js
@@ -2,11 +2,6 @@ const rollDice = require("../helpers/rollDice");
 
 // commande tir arbalète de chasse à une distance x avec un modificateur de tir y !abc x y
 
-// !!! A AJOUTER PLUS TARD !!!
-// le tableau est donné pour un nombre fini de distances
-// pour une distance intermédiaire (par ex 17 mètres),
-// faire la moyenne entre les deux distances encadrantes (15 et 20)
-
 // pour l'instant, seule la commande !abc est créée
 // il faudrait créer la commande !tir qui marche avec toutes les armes
 // arc court, long, composite, arbalète légère, de chasse, lourde, arquebuse, pistolet, fronde...
@@ -15,7 +10,7 @@ const rollDice = require("../helpers/rollDice");
 
 module.exports = {
   name: "abc",
-  help: "ceci est un test",
+  help: "Tir à l'arbalète de chasse",
   function: function (arguments) {
 
     // préambule : tableau des degrés de difficulté du tir
@@ -25,63 +20,65 @@ module.exports = {
     var id_table = new Array(2,2,2,4,4,4,5,5,6,6);
 
     var reply = "";
-//    var results = [];
 
-//    var [x, y] = arguments;  // méthode non robuste
-    // méthode alternative: ignore tout ce qui est écrit après les deux premiers nombres
-    var arg_join = arguments.join(" ");
-    var [dist, modif] = arg_join.split(" ",2);
+    var [dist, modif] = arguments;
+//    var arg_join = arguments.join(" ");
+//    var [dist, modif] = arg_join.split(" ",2);
 
     // vérifier que l'on a deux nombres
-    if (parseInt(dist) && parseInt(modif)) {
-      // tirage du d100
-      var score_de = rollDice("1d100").sum;
-      // résultat du test de tir
-      var test_tir = score_de + parseInt(modif);
-      reply += "Résultat du test: " + test_tir + " (dé=" + score_de + ")\n";
-      // gestion des critiques
-      if (score_de < 6) {
-        reply += "Échec critique\n";
-      } else if (score_de > 95) {
-        reply += "Succès critique\n";
-      }
-      // lire dd en fonction de la distance
-      var ind = 0;  // indice lecture tableau
-      while (dist > dist_table[ind]) {
-        ind++;
-      }
-      reply += "Degré de difficulté: " + dd_table[ind];
-      // analyse du résultat (échec ou réussite)
-      if (test_tir >= dd_table[ind]) {
-        var reussite = test_tir - dd_table[ind];
-        reply += " (dépassé de " + reussite + ")\n";
-        // division euclidienne pour obtenir le chiffre des dizaines
-        // ne pas dépasser la taille du tableau
-        var dizaine = Math.min(Math.floor(reussite/10) , 9);
-        var inddegats = id_table[dizaine]
-        reply += "Valeur blessure: " + inddegats + " (blessure ";
-        // entravante grave mortelle fatale
-        switch(inddegats) {
-          case 2:
-            reply += " entravante)\n";
-            break;
-          case 4:
-            reply += " grave)\n";
-            break;
-          case 5:
-            reply += " mortelle)\n";
-            break;
-          case 6:
-            reply += " fatale)\n";
-            break;
+    if (parseInt(dist) >= 0 && typeof parseInt(modif) === "number") {
+      if (dist <= 100) {
+        // tirage du d100
+        var score_de = rollDice("1d100").sum;
+        // résultat du test de tir
+        var test_tir = score_de + parseInt(modif);
+        reply += "Résultat du test: " + test_tir + " (dé=" + score_de + ")\n";
+        // gestion des critiques
+        if (score_de < 6) {
+          reply += "Échec critique\n";
+        } else if (score_de > 95) {
+          reply += "Succès critique\n";
+        }
+        // lire dd en fonction de la distance
+        var ind = 0;  // indice lecture tableau
+        while (dist > dist_table[ind]) {
+          ind++;
+        }
+        reply += "Degré de difficulté: " + dd_table[ind];
+        // analyse du résultat
+        if (test_tir >= dd_table[ind]) {  // réussite du tir
+          var reussite = test_tir - dd_table[ind];
+          reply += " (dépassé de " + reussite + ")\n";
+          // division euclidienne pour obtenir le chiffre des dizaines (ne pas dépasser la taille du tableau)
+          var dizaine = Math.min(Math.floor(reussite/10) , 9);
+          var inddegats = id_table[dizaine]
+          reply += "Valeur blessure: " + inddegats + " (blessure ";
+          // entravante grave mortelle fatale
+          switch(inddegats) {
+            case 2:
+              reply += " entravante)\n";
+              break;
+            case 4:
+              reply += " grave)\n";
+              break;
+            case 5:
+              reply += " mortelle)\n";
+              break;
+            case 6:
+              reply += " fatale)\n";
+              break;
           }
-      } else {
-        reply += "\n  Échec du tir\n";
+        } else {  // échec du tir
+          reply += "\n  Échec du tir\n";
+        }
+      } else {  // distance >100
+        reply += "Distance trop grande, situation non prévue\n";
+        return
       }
     }
 
     else {  // échec lecture commande
-      reply += "Commande non valide : (!abc distance modificteur)\n  ex: !abc 30 44"
+      reply += "Commande non valide : (!abc distance modificteur)\n  ex: !abc 30 44";
       return reply
     }
 
