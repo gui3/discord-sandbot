263034b0280660dc27af48920b871dff5e604be1
psi_thorn <guillon.corentin@gmail.com>
2020-10-11 12:12:40 +0200

    fct calc : affichage somme si plusieurs dé identiques

--- commands/calc.js
+++ commands/calc.js
@@ -7,17 +7,16 @@ module.exports = {
         "exple: *1d40 +5 /2d20*",
   function: function (arguments) {
     var precision = 2   // nombre de chiffres apres la virgule
+    var power = 10**precision;
     var results = []
     var reponse = ""
+    var total
     var calcString = arguments.join(" ")
 //    var diceRex = /\d+d\d+/
     var diceRex = /\d*d\d+/    // version modifié par Corentin
-    // + : Correspond à l'expression précédente qui est répétée une ou plusieurs fois
-    // ? : Correspond à l'expression précédente qui est présente une fois ou pas du tout
-    // * : Correspond à l'expression précédente qui est répétée 0 ou plusieurs fois
     // \d : Correspond à un nombre
-    // ^ : Correspond au début de la séquence
-    // . : Correspond à n'importe quel caractère excepté un caractère de saut de ligne
+    // + : Correspond à l'expression précédente qui est répétée une ou plusieurs fois
+    // * : Correspond à l'expression précédente qui est répétée 0 ou plusieurs fois
     // ici je mets '*' au lieu du '+' => si il n'y a aucun chiffre avant
     // avant le 'd', le prog ajoutera automatiquement un '1'
     // 'd100' est traité comme '1d100'
@@ -28,22 +27,28 @@ module.exports = {
       calcString = calcString.replace(dice, diceResult.sum);
     }
     if (!calcString.match(/^[ \d\.+-/*\(\)]+$/)) {
+      // ^ : Correspond au début de la séquence
+      // $ : Correspond à la fin de la séquence
+      // . : Correspond à n'importe quel caractère excepté un caractère de saut de ligne
       return "Calcul invalide, essayerais tu de me hacker ?";
     }
     else {
-      result = eval(calcString);
-    }
-    if (results.length > 0) {
-      reponse += "Résultats des dés :"
-      results.forEach(r => {
-        reponse += "\n" + r.string +" : "+r.dices
-      })
-      reponse += "\nCalcul intermédiaire : "+calcString+"\n";
-    }
+      total = Math.round(eval(calcString)*power)/power;
 
-    var power = 10**precision;
-    result = Math.round(result*power)/power;
-    reponse += "Résultat : **"+ result + "**\n";
-    return reponse
+      if (results.length > 0) {
+        reponse += "Résultats des dés :"
+        results.forEach(r => {
+          reponse += "\n" + r.string + " : " + r.dices
+          if (r.dices.length > 1) {
+            reponse += " (="+ r.sum + ")"
+          }
+        })
+        reponse += "\nCalcul intermédiaire : "+calcString+"\n";
+        reponse += "Résultat : **"+ total + "**\n";
+        return reponse
+      } else {
+        return "Commande vide"
+      }
+    }
   }
 };
